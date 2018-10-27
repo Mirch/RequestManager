aa6fd10c7d3ffc0a3532ad6c41f87a5af223617f
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-10-27 23:53:44 +0200

    Add Content-type to StringContent

--- RequestManager/Translation/TranslatorExtensions.cs
+++ RequestManager/Translation/TranslatorExtensions.cs
@@ -7,9 +7,19 @@ namespace SWriter.RequestManager.Translation
 {
     public static class TranslatorExtensions
     {
-        public static StringContent ToStringContent(this string str)
+        public static StringContent ToStringContent(this string str, ContentType contentType)
         {
-            return new StringContent(str);
+            string content = "";
+            switch(contentType)
+            {
+                case ContentType.JSON:
+                    content = "application/json";
+                    break;
+                case ContentType.XML:
+                    content = "application/xml";
+                    break;
+            }
+            return new StringContent(str, Encoding.UTF8, content);
         }
     }
 }
