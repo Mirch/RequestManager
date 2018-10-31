22988ccefe29d6f6d8dc0d6d161620ce961e266b
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-10-31 22:28:16 +0100

    Create Sandbox project

---
+++ Sandbox/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sandbox.Models;
+using SWriter.RequestManager;
+
+namespace Sandbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RequestSender _requestSender;
+
+        public HomeController()
+        {
+            _requestSender = new RequestSender("http://localhost:9453/");
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var getJsonResponse = (await _requestSender.GetAsync("/api/test/json")).ResultAs<Person>(ContentType.JSON);
+            ViewData["getJsonResponse"] = getJsonResponse;
+
+            var getXmlResponse = (await _requestSender.GetAsync("/api/test/xml")).ResultAs<Person>(ContentType.XML);
+            ViewData["getXmlResponse"] = getXmlResponse;
+
+            var postAndGetJson = (await _requestSender.PostAsync("/api/test/json", new Person() { Id = 2, Name = "John" }))
+                                    .ResultAs<Person>(ContentType.JSON);
+            ViewData["postAndGetJson"] = postAndGetJson;
+
+            var postAndGetXml = (await _requestSender.PostAsync("/api/test/xml", new Person() { Id=2, Name="John"})).ResultAs<Person>(ContentType.XML);
+            ViewData["postAndGetXml"] = postAndGetXml;
+
+            var postFormGetJson = (await _requestSender.PostAsync("/api/test/form", new Person() { Id = 2, Name = "John" }, ContentType.FORM)).ResultAs<Person>();
+            ViewData["postFormGetJson"] = postFormGetJson;
+
+
+            return View();
+        }
+    }
+}
---
+++ Sandbox/Controllers/TestController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sandbox.Models;
+
+namespace Sandbox.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet("xml")]
+        [Produces("application/xml")]
+        [FormatFilter]
+        public IActionResult TestGetXml()
+        {
+            return Ok(new Person() { Id = 1, Name = "Test" });
+        }
+
+        [HttpGet("json")]
+        [Produces("application/json")]
+        public IActionResult TestGetJson()
+        {
+            return Ok(new Person() { Id = 1, Name = "Test" });
+        }
+
+        [HttpPost("xml")]
+        [Produces("application/xml")]
+        public IActionResult TestPostXml([FromBody]Person model)
+        {
+            return Ok(model);
+        }
+
+        [HttpPost("json")]
+        [Produces("application/json")]
+        public IActionResult TestPostJson([FromBody]Person model)
+        {
+            return Ok(model);
+        }
+
+        [HttpPost("form")]
+        [Produces("application/json")]
+        public IActionResult TestPostForm([FromForm]IFormCollection form)
+        {
+            var person = new Person() { Id=Convert.ToInt32(form["id"]), Name=form["name"]};
+            return Ok(person);
+        }
+
+    }
+}
